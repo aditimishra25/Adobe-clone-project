599db3713e39eb9e0e4cc54b6e90d299a94fa4bf
Aditi <aditimishra2501@gmail.com>
2023-03-30 12:40:56 +0530

    removed unwanted files

--- src/components/faq/FAQ.jsx
+++ src/components/faq/FAQ.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import questions from "./../../faq.json";
 import FAQS from '../../containers/faqs/FAQS';
 
-// export function FAQ() {
-//   return (
-  const FAQ = ({ question, answer }) => (
-    <FAQS>
+const FAQ = ({ question, answer }) => (
+      <FAQS>
       <FAQS.Header>FAQ</FAQS.Header>
       {questions.map((question) => (
         <FAQS.Entity key={question.id}>
@@ -15,18 +13,5 @@ import FAQS from '../../containers/faqs/FAQS';
       ))}
     </FAQS>
   );
-
-// const Feature = ({ title, text }) => (
-//   <div className="gpt3__features-container__feature">
-//     <div className="gpt3__features-container__feature-title">
-//       <div />
-//       <h1>{title}</h1>
-//     </div>
-//     <div className="gpt3__features-container_feature-text">
-//       <p>{title}</p>
-//       <p>{text}</p>
-//     </div>
-//   </div>
-// );
 
 export default FAQ;
